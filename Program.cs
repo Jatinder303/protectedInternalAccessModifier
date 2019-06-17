ab68cce2be55966844061c9eb13f073376368e97
Jatinder Singh <Jatinder.Singh@atcltd.atc.org.nz>
2019-06-17 14:05:06 +1200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿
+using ProjectParent;
+using System;
+
+namespace protectedInternalAccessModifier
+{
+    class Program : Parent
+    {
+        // Main Method
+        public static void Main(string[] args)
+        {
+            // Accessing value in another assembly 
+            Program obj3 = new Program();
+
+            // Member value is Accessible 
+            obj3.value = 9;
+            Console.WriteLine("Value = " + obj3.value);
+        }
+    }
+}
